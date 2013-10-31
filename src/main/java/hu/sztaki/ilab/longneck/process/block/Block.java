f091571f5f7351b797480540fdc37aa5f5f82f56
sidlo <csaba.sidlo@gmail.com>
2013-10-31 14:26:39 +0100

    Minor fixes with some documentation added.

--- src/main/java/hu/sztaki/ilab/longneck/process/block/Block.java
+++ src/main/java/hu/sztaki/ilab/longneck/process/block/Block.java
@@ -4,21 +4,22 @@ import hu.sztaki.ilab.longneck.Record;
 import hu.sztaki.ilab.longneck.process.*;
 
 /**
- * Transformation block.
- * 
+ * Blocks map transformation rules to executable code: every transformation step
+ * is a block of transformation.
+ *
  * @author Molnar Peter <molnarp@sztaki.mta.hu>
  */
 public interface Block extends Cloneable, SourceInfoContainer {
-    
+
     /**
      * Applies the transformation to the specified record.
-     * 
+     *
      * This method is a mutator.
-     * 
+     *
      * @param record The record that is transformed.
      */
-    public void apply(Record record, VariableSpace parentScope) 
+    public void apply(Record record, VariableSpace parentScope)
             throws CheckError, BreakException, FailException, FilterException;
-    
+
     public Block clone();
 }
