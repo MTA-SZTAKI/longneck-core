710f798766df01c6856ea71e1d9302a6feda998f
Dome Geszler <gdome@info.ilab.sztaki.hu>
2014-03-25 16:57:10 +0100

    test-cases,test,record,field mapping wip 1

---
+++ src/main/java/hu/sztaki/ilab/longneck/TestCase.java
@@ -0,0 +1,18 @@
+package hu.sztaki.ilab.longneck;
+
+import java.util.List;
+
+public class TestCase {
+
+  private Record sourceRecord;
+  private List<Record> targetRecords;
+  private List<Record> errorTargetRecords;
+  
+  public Record getSourceRecord() {
+    return sourceRecord;
+  }
+  public void setSourceRecord(Record sourceRecord) {
+    this.sourceRecord = sourceRecord;
+  }
+
+}
