d2c6b38fb2a2b6cc871ae189ca44858c5d490e3f
Dome Geszler <gdome@info.ilab.sztaki.hu>
2014-03-26 12:30:49 +0100

    TestCase set source/target/error records

---
+++ src/main/java/hu/sztaki/ilab/longneck/RecordImplForTest.java
@@ -0,0 +1,29 @@
+package hu.sztaki.ilab.longneck;
+
+public class RecordImplForTest extends RecordImpl implements Record {
+
+  public enum Role {
+    SOURCE, TARGET, ERROR
+  };
+
+  private Role role;
+
+  public Role getRole() {
+    return role;
+  }
+
+  public String getRoleStr() {
+    return role.toString();
+  }
+  public void setRole(String roleStr) {
+    if (roleStr.equals("source")) {
+      role = Role.SOURCE;
+    } else if (roleStr.equals("target")) {
+      role = Role.TARGET;
+    } else if (roleStr.equals("error-target")) {
+      role = Role.ERROR;
+    } else {
+      throw new RuntimeException();
+    }
+  }
+}
--- src/main/java/hu/sztaki/ilab/longneck/TestCase.java
+++ src/main/java/hu/sztaki/ilab/longneck/TestCase.java
@@ -1,18 +1,41 @@
 package hu.sztaki.ilab.longneck;
 
+import hu.sztaki.ilab.longneck.RecordImplForTest.Role;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestCase {
 
+  private List<Record> records = new ArrayList<Record>();
   private Record sourceRecord;
-  private List<Record> targetRecords;
-  private List<Record> errorTargetRecords;
-  
+  private List<Record> targetRecords = new ArrayList<Record>();
+  private List<Record> errorTargetRecords = new ArrayList<Record>();
+
   public Record getSourceRecord() {
     return sourceRecord;
   }
+
+  public void addRecord(RecordImplForTest record) {
+    records.add(record);
+    if (record.getRole().equals(Role.SOURCE)) {
+      setSourceRecord(record);
+    } else if (record.getRole().equals(Role.TARGET)) {
+      addTargetRecord(record);
+    } else if (record.getRole().equals(Role.ERROR)) {
+      addErrorTargetRecord(record);
+    } 
+  }
+
   public void setSourceRecord(Record sourceRecord) {
     this.sourceRecord = sourceRecord;
   }
 
+  public void addTargetRecord(Record record) {
+    targetRecords.add(record);
+  }
+
+  public void addErrorTargetRecord(Record record) {
+    errorTargetRecords.add(record);
+  }
 }
