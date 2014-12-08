1a8b2db53617e59854b334a5e04b8b5ce3b49ef2
Csaba Sidló <csaba.sidlo@gmail.com>
2014-12-08 13:48:12 +0100

    DatabaseSource initialization logging fixed.

--- src/main/java/hu/sztaki/ilab/longneck/process/access/DatabaseSource.java
+++ src/main/java/hu/sztaki/ilab/longneck/process/access/DatabaseSource.java
@@ -9,11 +9,11 @@ import org.apache.log4j.Logger;
 
 /**
  * JDBC SQL source implementation.
- * 
+ *
  * @author Molnár Péter <molnarp@sztaki.mta.hu>
  */
 public class DatabaseSource extends AbstractDatabaseAccessor implements Source {
-    
+
     /** The log to write to. */
     private final static Logger log  = Logger.getLogger(DatabaseSource.class);
     /** The sql query to retrieve records. */
@@ -22,14 +22,14 @@ public class DatabaseSource extends AbstractDatabaseAccessor implements Source {
     private boolean endOfTable = false;
     /** The class to use as a record. */
     private Class<Record> recordClass;
-    
+
     private Connection connection = null;
     /** Currently read result set. */
     private ResultSet resultSet = null;
     /** The column names from the query. */
     private List<String> columns = null;
-    
-    
+
+
     @Override
     public void init() {
         try {
@@ -37,7 +37,7 @@ public class DatabaseSource extends AbstractDatabaseAccessor implements Source {
             if (recordClass == null) {
                 setRecordClass("hu.sztaki.ilab.longneck.RecordImpl");
             }
-            
+
             // Get SQL connection
             connection = dataSource.getConnection();
 
@@ -55,35 +55,38 @@ public class DatabaseSource extends AbstractDatabaseAccessor implements Source {
                 columns.add(metaData.getColumnName(i));
             }
         } catch (SQLException ex) {
+            log.error("initializing database source failed for SQL:\n" + query, ex);
             throw new RuntimeException(ex);
         }
     }
-    
+
     @Override
     public void close() {
         try {
-            resultSet.close();
-            resultSet = null;
+            if ( resultSet != null && ! resultSet.isClosed() ) {
+                resultSet.close();
+                resultSet = null;
+            }
             connection = null;
             columns = null;
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
     }
-    
+
     @Override
-    public Record getRecord() throws NoMoreRecordsException {        
-        try {                        
+    public Record getRecord() throws NoMoreRecordsException {
+        try {
             //TODO lbendig, Feb 28, 2012: quick fix for DB source initialization
             if (resultSet == null) {
                 init();
             }
-            
+
             boolean hasNext = resultSet.next();
             if (endOfTable || ! hasNext) {
-                log.debug(String.format("End of table : %1$b, next: %2$b, sending shutdown record.", 
+                log.debug(String.format("End of table : %1$b, next: %2$b, sending shutdown record.",
                         endOfTable, hasNext));
-                
+
                 endOfTable = true;
                 throw new NoMoreRecordsException();
             }
@@ -93,19 +96,19 @@ public class DatabaseSource extends AbstractDatabaseAccessor implements Source {
             for (int i = 0; i < columns.size(); ++i) {
                 r.add(new Field(columns.get(i), resultSet.getString(i + 1)));
             }
-            
+
             return r;
-            
+
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         } catch (InstantiationException ex) {
             throw new AssertionError(ex);
         } catch (IllegalAccessException ex) {
             throw new AssertionError(ex);
-        }            
-        
-    }   
-    
+        }
+
+    }
+
     public String getQuery() {
         return query;
     }
@@ -113,16 +116,16 @@ public class DatabaseSource extends AbstractDatabaseAccessor implements Source {
     public void setQuery(String query) {
         this.query = query;
     }
-    
+
     public void setRecordClass(String className) {
         try {
             recordClass = (Class<Record>) Class.forName(className);
         } catch (ClassNotFoundException ex) {
             throw new RuntimeException(ex);
         } catch (ClassCastException ex) {
-            throw new RuntimeException(String.format("Class %1$s does not implement %2$s.", 
+            throw new RuntimeException(String.format("Class %1$s does not implement %2$s.",
                     className, Record.class.getName()), ex);
         }
     }
-        
+
 }
